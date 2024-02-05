50fab4b1f0a5ead47a24926476a96899dac4eaeb
mtm283 <mtm283@scarletmail.rutgers.edu>
2024-02-04 23:07:21 -0500

    Points for Each Apple
    Scripted so points for each apple caught would be added to score.

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; // This line enables use of uGUI features. 
 
 public class Basket : MonoBehaviour
 {
+    [Header("Set Dynamically")]
+    public Text scoreGT; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+
+        // Get the Text Component of that GameObject
+        scoreGT = scoreGO.GetComponent<Text>();
+
+        // Set the starting number of points to 0
+        scoreGT.text = "0";
     }
 
     // Update is called once per frame
@@ -30,11 +40,20 @@ public class Basket : MonoBehaviour
 
     void OnCollisionEnter(Collision coll)
     {
-        //Find out what hit this basket 
+        // Find out what hit this basket 
         GameObject collideWith = coll.gameObject; 
         if (collideWith.tag == "Apple")
         {
             Destroy(collideWith);
         }
+
+        // Parse the text of the scoreGT into an int
+        int score = int.Parse(scoreGT.text);
+
+        // Add points for catching the apple
+        score += 100;
+
+        // Convert the score back to a string and display it 
+        scoreGT.text = score.ToString();
     }
 }
