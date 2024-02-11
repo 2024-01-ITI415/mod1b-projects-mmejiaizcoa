88999f4c0f6ff8da03c68c20726dbb52b8cd6ed0
mtm283 <mtm283@scarletmail.rutgers.edu>
2024-02-11 08:34:08 -0500

    Adjusting Shots
    It detects when the ball is moving under a certain amount then sends you back to sling another ball.

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -33,12 +33,38 @@ public class FollowCam : MonoBehaviour
     void FixedUpdate()
     {
         // If there's only one line following an if, it doesn't need braces 
-        if (POI == null) return;
+        // if (POI == null) return;
 
-        // Get the position of the poi 
-        Vector3 destination = POI.transform.position;
+        // Get the position of the poi
+        // Vector3 destination = POI.transform.position;
 
-        //Limit the X & Y to minimum values 
+        Vector3 destination;
+
+        // If there is no poi, return to P: [0, 0, 0]
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else
+        {
+            // Get the position of the poi 
+            destination = POI.transform.position;
+            // If poi is Projectile, check to see if it it's at rest 
+            if (POI.tag == "Projectile")
+            {
+                // If it is sleeping (not moving)
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    // Return to default view
+                    POI = null;
+
+                    // In the next update
+                    return;
+                }
+            }
+        }
+
+        // Limit the X & Y to minimum values 
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
 
