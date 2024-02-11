da7f55ab5f8247e4fa5233b5f35b881ef657ae59
mtm283 <mtm283@scarletmail.rutgers.edu>
2024-02-11 09:15:16 -0500

    Projectile Line
    Set up the projectile line and wrote out script

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Slingshot : MonoBehaviour
 
 {
+    static private Slingshot S;
+
 // fields set in the Unity Inspector Pane
     [Header("Set in Inspector")]
 
@@ -18,9 +20,19 @@ public class Slingshot : MonoBehaviour
     public GameObject projectile;
     public bool aimingMode;
     private Rigidbody projectileRigidbody;
-    
+
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+   
     void Awake()
     {
+        S = this;
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
